6c5def3656630fd43c5503650e58461cbd25e852
Millagoss <millagoss19@gmail.com>
2022-12-15 12:57:08 +0300

    reduxToolkit setup complete, userSlice created, working on userAsyncThunk

---
+++ src/store/features/user/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+import { toast } from 'react-toastify';
+
+const initialState = {
+  user: null,
+  isLoading: false,
+};
+
+export const registerUser = createAsyncThunk(
+  'user/register-user',
+  async (userInfo, thunkApi) => {
+    console.log(userInfo);
+  }
+);
+export const loginUser = createAsyncThunk(
+  'user/login-user',
+  async (userInfo, thunkApi) => {
+    console.log(userInfo);
+  }
+);
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+});
+
+export default userSlice.reducer;
