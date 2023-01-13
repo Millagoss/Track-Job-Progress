3de8d2688edc0618ff47617c0349e3d4d9753e82
Millagoss <millagoss19@gmail.com>
2023-01-13 21:54:44 +0300

    allJobsSlice created, setting the structure of allJobPage.

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import userSlice from './features/user/userSlice';
 import addJobSlice from './features/Job/addJobSlice';
+import allJobsSlice from './features/allJobs/allJobsSlice';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
     addJob: addJobSlice,
+    allJobs: allJobsSlice,
   },
 });
 
