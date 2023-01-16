88967611661610ec0ad9c85a7464af74b679a2e8
Millagoss <millagoss19@gmail.com>
2023-01-16 20:09:18 +0300

    editJob functionality added, user can modify there job.

--- src/store/features/Job/addJobSlice.js
+++ src/store/features/Job/addJobSlice.js
@@ -1,12 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
-import customFetch from '../../../utils/axios/axios';
-import { logoutUser } from '../user/userSlice';
 import {
-  showLoading,
-  hideLoading,
-  fetchJobsAsyncThunk,
-} from '../allJobs/allJobsSlice';
+  addJobAsyncThunk,
+  deleteJobAsyncThunk,
+  editJobAsyncThunk,
+} from './addJobThunk';
 
 const initialState = {
   isLoading: false,
@@ -23,42 +21,16 @@ const initialState = {
 
 export const addJob = createAsyncThunk(
   'Job/addJob',
-  async (JobInfo, thunkApi) => {
-    try {
-      const resp = await customFetch.post('/jobs', JobInfo, {
-        headers: {
-          authorization: `Bearer ${thunkApi.getState().user.user.token}`,
-        },
-      });
-      thunkApi.dispatch(clearForm());
-      return resp.data;
-    } catch (error) {
-      if (error.response.status === 401) {
-        thunkApi.dispatch(logoutUser('UNauthorized request!!'));
-        return;
-      }
-      return thunkApi.rejectWithValue(error.response.data.msg);
-    }
-  }
+  async (JobInfo, thunkApi) => addJobAsyncThunk(JobInfo, thunkApi, clearForm)
 );
 
 export const deleteJob = createAsyncThunk(
   'Job/deleteJob',
-  async (jobId, thunkApi) => {
-    thunkApi.dispatch(showLoading());
-    try {
-      const resp = await customFetch.delete(`/jobs/${jobId}`, {
-        headers: {
-          authorization: `Bearer ${thunkApi.getState().user.user.token}`,
-        },
-      });
-      thunkApi.dispatch(fetchJobsAsyncThunk());
-      return resp.data;
-    } catch (error) {
-      thunkApi.dispatch(hideLoading());
-      return thunkApi.rejectWithValue(error.response.data.msg);
-    }
-  }
+  async (jobId, thunkApi) => deleteJobAsyncThunk(jobId, thunkApi)
+);
+
+export const editJob = createAsyncThunk('Job/editJob', async (info, thunkApi) =>
+  editJobAsyncThunk(info, thunkApi)
 );
 
 const addJobSlice = createSlice({
@@ -71,6 +43,9 @@ const addJobSlice = createSlice({
     },
     clearForm: () => {
       return { ...initialState };
+    },
+    setEditJob: (state, { payload }) => {
+      return { ...state, isEditing: true, ...payload };
     },
   },
   extraReducers: {
@@ -85,15 +60,27 @@ const addJobSlice = createSlice({
       state.isLoading = false;
       toast.error(payload);
     },
-    //////////////////////// deleteJob
+    //////////////////////////////////////////////////// deleteJob
     [deleteJob.fulfilled]: (_, { payload }) => {
       toast.success(payload.msg);
     },
     [deleteJob.rejected]: (_, { payload }) => {
       toast.error(payload);
     },
+    // editJob ///////////////////////////////////////////////////
+    [editJob.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [editJob.fulfilled]: (state) => {
+      state.isLoading = false;
+      toast.success('Job Updated Successfully');
+    },
+    [editJob.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
   },
 });
 
 export default addJobSlice.reducer;
-export const { changeHandler, clearForm } = addJobSlice.actions;
+export const { changeHandler, clearForm, setEditJob } = addJobSlice.actions;
